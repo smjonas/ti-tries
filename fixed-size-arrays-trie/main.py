e721dbe1de9ff392ae4ff582fc066414b40f39f9
smjonas <jonas.strittmatter@gmx.de>
2024-11-24 22:50:55 +0100

    Save results + take measurements

--- fixed-size-arrays-trie/main.py
+++ fixed-size-arrays-trie/main.py
@@ -1,4 +1,6 @@
+import resource
 import sys
+import time
 from dataclasses import dataclass
 from typing import Optional
 
@@ -9,9 +11,9 @@ ALPHABET_SIZE = 26 + 26 + 10 + 1
 def char_to_index(ch: str) -> Optional[int]:
     if ch == "\0":
         return ALPHABET_SIZE - 1
-    elif "A" <= ch <= "Z":
-        return ord(ch) - ord("A")
     elif "a" <= ch <= "z":
+        return ord(ch) - ord("A")
+    elif "A" <= ch <= "Z":
         return ord(ch) - ord("a") + 26
     elif "0" <= ch <= "9":
         return ord(ch) - ord("0") + 26 + 26
@@ -22,11 +24,11 @@ def char_to_index(ch: str) -> Optional[int]:
 @dataclass
 class Node:
     children: list[Optional["Node"]]
-    is_eow: bool
+    is_leaf: bool
 
     def __init__(self):
         self.children = [None] * ALPHABET_SIZE
-        self.is_eow = True
+        self.is_leaf = True
 
 
 @dataclass
@@ -46,7 +48,7 @@ class Trie:
                 if child is None:
                     current.children[idx] = Node()
                     child = current.children[idx]
-                    current.is_eow = False
+                    current.is_leaf = False
                     if is_last_char:
                         return True
                 current = child
@@ -65,7 +67,7 @@ class Trie:
             if child is None:
                 return False
             current = child
-        return current.is_eow
+        return current.is_leaf
 
     def delete(self, word: str) -> bool:
         current = self.root
@@ -86,7 +88,7 @@ class Trie:
         parent.children[idx_to_delete] = None
         # Parent only has empty children
         if all(child is None for child in parent.children):
-            parent.is_eow = True
+            parent.is_leaf = True
         return True
 
 
@@ -95,25 +97,41 @@ if __name__ == "__main__":
     query_file_path = sys.argv[2]
     with open(input_file_path, "r") as input_file:
         input_contents = input_file.read()
-    input_words = [word for word in input_contents.split("\n") if word != ""]
-    print(input_words)
+    with open(query_file_path, "r") as query_file:
+        query_contents = query_file.read()
+    output_file_path = f"result_{input_file_path.split('/')[-1]}"
+    input_words = input_contents.split("\n")[:-1]
+    query_lines = query_contents.split("\n")[:-1]
+
+    start_time = time.time()
     trie = Trie()
     for word in input_words:
         trie.insert(word)
-    with open(query_file_path, "r") as query_file:
-        query_contents = query_file.read()
-    query_lines = query_contents.split("\n")
+    trie_construction_time_end = time.time()
+
+    results = []
     for query_line in query_lines:
-        if query_line == "":
-            continue
         query_word, query_type = query_line.split(" ")
         match query_type:
             case "i":
-                print(trie.insert(query_word))
+                results.append(trie.insert(query_word))
             case "c":
-                print(trie.contains(query_word))
+                results.append(trie.contains(query_word))
             case "d":
-                print(trie.delete(query_word))
+                results.append(trie.delete(query_word))
             case _:
                 raise ValueError(f"Invalid query type '{query_type}'")
-    print(query_lines)
+
+    query_time_end = time.time()
+    with open(output_file_path, "w") as output_file:
+        for result in results:
+            output_file.write(("true" if result else "false") + "\n")
+    trie_construction_time_ms = (trie_construction_time_end - start_time) * 1000
+    query_time_ms = (query_time_end - start_time) * 1000
+    trie_construction_memory_kb = resource.getrusage(resource.RUSAGE_SELF).ru_maxrss
+    # Convert kilobytes to mebibytes
+    trie_construction_memory_mib = (trie_construction_memory_kb * 1000) / 1_048_576
+    print(
+        f"RESULT name=Jonas-Strittmatter trie_construction_time={trie_construction_time_ms:.4f} "
+        f"trie_construction_memory={trie_construction_memory_mib:.4f} query_time={query_time_ms:.4f}"
+    )
