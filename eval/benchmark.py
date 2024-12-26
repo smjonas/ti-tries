275487c93c40010e5e6123f464df47c804f356c9
smjonas <jonas.strittmatter@gmx.de>
2024-12-26 22:46:30 +0100

    Add script to run benchmarks

---
+++ eval/benchmark.py
@@ -0,0 +1,51 @@
+import re
+import subprocess
+import sys
+
+
+def parse_result(result_string: str, pattern: str) -> dict:
+    matches = re.findall(pattern, result_string)
+    values = {}
+    for key, value in matches:
+        values[key] = float(value)
+    return values
+
+
+def benchmark_project(project_name, input_file, query_file, output_file):
+    command = f"python3 ../{project_name}/main.py {input_file} {query_file} {output_file}"
+    result = subprocess.run(command, shell=True, capture_output=True, text=True)
+    if result.returncode != 0:
+        sys.exit(result.stderr)
+    return parse_result(result.stdout, r"(\w+)=([\d.]+)")
+
+
+def print_result(project_name, input_file, query_file, output_file):
+    input_params = parse_result(input_file, r"(\w)-([\d.]+)")
+    n, l = input_params["n"], input_params["l"]
+    results = benchmark_project(project_name, input_file, query_file, output_file)
+    construction_time, construction_memory, query_time = (
+        results["trie_construction_time"],
+        results["trie_construction_memory"],
+        results["query_time"],
+    )
+    print(
+        f"{n},{l},{input_file},{construction_time},{construction_memory},{query_time}"
+    )
+
+
+def print_header():
+    print("n,l,input_file,construction_time,construction_memory,query_time")
+
+
+if __name__ == "__main__":
+    args = [arg for arg in sys.argv[1:] if not arg.startswith("--")]
+    flags = [arg for arg in sys.argv[1:] if arg.startswith("--")]
+    if "--header" in flags:
+        print_header()
+        sys.exit(0)
+
+    if len(sys.argv) != 5:
+        print("Usage: python <project_name> <input_file> <query_file> <output_file> [--header]")
+        sys.exit(1)
+
+    print_result(*args)
--- eval/run_experiments.py
+++ eval/run_experiments.py
@@ -1,13 +1,27 @@
 import run
 
-num_words = 1000
+num_words_max = 1000
+step_size = 100
 word_length = 10
 
+assert num_words_max % step_size == 0
+
+input_file_path = f"./dataset/trie_n-[[num_words]]_l-{word_length}.txt"
+num_words = [step_size + i * step_size for i in range(num_words_max // step_size)]
 run.add(
     "gen_dataset",
-    f"python3 gen_dataset.py {num_words} {word_length} ./dataset/trie_n-{num_words}_l-{word_length}.txt",
-    {},
-    creates_file=f"./dataset/trie_n-{num_words}_l-{word_length}.txt",
+    f"python3 gen_dataset.py [[num_words]] {word_length} {input_file_path}",
+    {"num_words": num_words},
+    creates_file=input_file_path,
+)
+
+project = "fixed-size-arrays-trie"
+run.add(
+    "benchmark",
+    f"python3 benchmark.py {project} {input_file_path} ../input/empty-queries.txt ../output/tmp-results.txt",
+    {"num_words": num_words},
+    header_command="python3 benchmark.py --header",
+    stdout_file=f"./output/benchmark-results-{project}.csv",
 )
 
 run.run()
