f0ce6594ff506121360c8af040531c60943d1ec4
smjonas <jonas.strittmatter@gmx.de>
2025-01-26 18:50:09 +0100

    Add functional tests

--- src/test_tries.py
+++ src/test_tries.py
@@ -1,4 +1,23 @@
+import os
 import unittest
+from typing import Callable
+
+
+def test_scenario_for_project(self, variant, project_name: str, build_trie: Callable):
+    from base import run
+
+    self.assertTrue(os.path.exists("../output/example-expected-output.txt"))
+    run(
+        variant,
+        "../input/example-input.txt",
+        "../input/example-queries.txt",
+        f"../output/results-{project_name}.txt",
+        build_trie,
+    )
+    with open("../output/example-expected-output.txt") as expected_file, open(
+        f"../output/results-{project_name}.txt"
+    ) as actual_file:
+        self.assertListEqual(list(expected_file), list(actual_file))
 
 
 class TestBalancedSearchTreeTrie(unittest.TestCase):
@@ -19,8 +38,13 @@ class TestBalancedSearchTreeTrie(unittest.TestCase):
         self.assertTrue(self.trie.delete("apple"))
         self.assertFalse(self.trie.contains("apple"))
 
+    def test_scenario(self):
+        from balanced_search_tree_trie import Trie
 
-class TestFixedSizeArrayTrie(unittest.TestCase):
+        test_scenario_for_project(self, 3, "balanced-search-tree-trie", lambda: Trie())
+
+
+class TestFixedSizeArraysTrie(unittest.TestCase):
     def setUp(self):
         from fixed_size_arrays_trie import Trie
 
@@ -38,8 +62,13 @@ class TestFixedSizeArrayTrie(unittest.TestCase):
         self.assertTrue(self.trie.delete("banana"))
         self.assertFalse(self.trie.contains("banana"))
 
+    def test_scenario(self):
+        from fixed_size_arrays_trie import Trie
 
-class TestHashTableTrie(unittest.TestCase):
+        test_scenario_for_project(self, 1, "fixed-size-arrays-trie", lambda: Trie())
+
+
+class TestHashTablesTrie(unittest.TestCase):
     def setUp(self):
         from hash_tables_trie import Trie
 
@@ -56,3 +85,8 @@ class TestHashTableTrie(unittest.TestCase):
         self.trie.insert("cherry")
         self.assertTrue(self.trie.delete("cherry"))
         self.assertFalse(self.trie.contains("cherry"))
+
+    def test_scenario(self):
+        from hash_tables_trie import Trie
+
+        test_scenario_for_project(self, 2, "hash-tables-trie", lambda: Trie())
